281a27a1fe48c01f7eda06ff75955ff6050e1f26
Travis Paul <tr@vispaul.me>
2019-07-22 22:30:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require('./package.json');
+
+// Options used by all commands
+const commonOptions = [
+    {
+        names: ['verbose', 'v'],
+        type: 'bool',
+        help: 'Write more info to stdout'
+    }
+];
+
+app.options = [
+    {
+        names: ['help', 'h'],
+        type: 'bool',
+        help: 'Print help and exit.'
+    }
+];
+
+app.cmdList = [];
+app.addCmd = (cmd) => {
+    app[cmd] = require(`./${cmd}`);
+    app.cmdList.push(cmd);
+    app[cmd].options = app[cmd].options.concat(commonOptions);
+};
+
+app.addCmd('search');
+
+module.exports = app;
---
+++ search.js
@@ -0,0 +1,43 @@
+const NVD = require('nvd');
+const ProgressBar = require('progress');
+
+function main (subcmd, options) {
+  if (!options._args.length) {
+    console.error('No CVE ID provided');
+    process.exitCode = 1;
+    return;
+  }
+  const id = options._args[0];
+  const nvd = new NVD();
+  const config = nvd.getConfig();
+  const bar = new ProgressBar('syncing [:bar]', config.feeds.length);
+
+  if (options.nosync) {
+    return nvd.search(id);
+  }
+
+  nvd.sync((error, results) => {
+    if (error) {
+      process.exitCode = 1;
+      return console.error(error);
+    }
+    nvd.search(id);
+  }, () => {
+    bar.tick();
+  });
+}
+
+module.exports = {
+  main,
+  options: [
+    {
+        names: ['nosync', 'n'],
+        type: 'bool',
+        help: 'Do not fetch remote meta files and attempt a sync operation'
+    }
+  ],
+  help: `Search for a CVE by ID.
+Usage:
+  {{name}} {{cmd}} [CVE ID]
+{{options}}`
+};
